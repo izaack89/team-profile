88a9842fe1401983c36126a28e80f0d6a31912c4
German Ramirez <mitsuominagi@gmail.com>
2021-05-18 15:50:42 -0500

    Added the intern section for the HTML output

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,8 +1,40 @@
 
+const managerSection = function (manager) {
+    return  ``;
+
+}
+
+const engineerSection = function (engineer) {
+    return `
+    `;
+
+}
+
+const internSection = function (intern) {
+    return `
+<div class="col-xs-2 col-sm-3 py-2">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header custom-background-color ">
+            <h4>${intern.name}</h4>
+            <p><i class="fas fa-graduation-cap"></i> Intern</p>
+        </div>
+
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID:${intern.id} </li>
+                <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+                <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
+        </div>
+    </div>
+</div>  
+    `;
+
+}
 
 const generateHTML = function (team) {
 
-    
+
  }
 
 // Here the function of generateHTML is exported 
