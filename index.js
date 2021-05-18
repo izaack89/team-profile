964658ca581da275c1460ef6191f29c9f7527fcb
German Ramirez <mitsuominagi@gmail.com>
2021-05-18 15:16:28 -0500

    Added code to determine what questions should display the inquirer

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Manager = require('./lib/Manager');
 
 // Declare the variable that will save the information of the team
 
-const team = []
+const teamInfo = []
 
 //I create the questions for the manager profile
 const questionsManager = [
@@ -80,7 +80,62 @@ const questionMember= [
         type: 'list',
         name: 'addMember',
         message: 'Which type of team member would you like to add',
-        choices: ['Engineer', 'Intern','I don\'t want to add any more team members'],
-        default: 'I don\'t want to add any more team members'
+        choices: ['Engineer', 'Intern','I don\'t want to add any more team members']
     }
 ];
+
+// I create a function to DRY and reuse the inquirer , but with the difference that I set a swtich to manage the actions depending on the part of the system 
+const inquirerDisplay = function (questions,afterActions) {
+    // I call inquirer, I set on the prompt the question that was defined before 
+inquirer
+    .prompt(questions)
+    .then(dataQuestions => {
+        switch (afterActions) {
+            case 'manager':
+                // I create an instance of the Manager class to storage the information providen on the questionnarie
+                const manager = new Manager(dataQuestions.managerId, dataQuestions.managerName, dataQuestions.managerEmail, dataQuestions.managerOfficeNumber);
+                // I save the instance of the class into an array , this will help me to use it when I print the HTML 
+                teamInfo.push(manager); 
+                // I call the question of add a Member once I saved the information of the Manager
+                inquirerDisplay(questionMember, 'addMember');
+            break;
+            case 'addMember':
+                if (dataQuestions.addMember === "Engineer") {
+                    // I call the question of the engineeer 
+                    inquirerDisplay(questionsEngineer, 'engineer');
+                } else if (dataQuestions.addMember === "Intern") {
+                    // I call the question of the engineeer 
+                    inquirerDisplay(questionsIntern, 'intern');
+                } else {
+                    // If this happend I will call the function that create the HTML file 
+                    console.log(teamInfo); 
+                }
+                
+                break;
+            case 'engineer':
+                // I create an instance of the Engineer class to storage the information providen on the questionnarie
+                const engineer = new Engineer(dataQuestions.engineerId, dataQuestions.engineerName, dataQuestions.engineerEmail, dataQuestions.engineerGithub);
+                // I save the instance of the class into an array , this will help me to use it when I print the HTML 
+                teamInfo.push(engineer);
+                // I call the question of add a Member once I saved the information of the Engineer
+                inquirerDisplay(questionMember, 'addMember');
+                break;
+            case 'intern':
+                // I create an instance of the Intern class to storage the information providen on the questionnarie
+                const intern = new Intern(dataQuestions.internId, dataQuestions.internName, dataQuestions.internEmail, dataQuestions.internSchool);
+                // I save the instance of the class into an array , this will help me to use it when I print the HTML 
+                teamInfo.push(intern);
+                // I call the question of add a Member once I saved the information of the Intern
+                inquirerDisplay(questionMember, 'addMember');
+                break;
+        }
+    });
+}
+
+
+const init = function () {
+    // I initialize the system to display the information of the manager 
+    inquirerDisplay(questionsManager, 'manager');
+}
+// Function call to initialize app
+init();
