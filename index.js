50f6212496f54136dc080eb0a44cf7f3cc96eff9
German Ramirez <mitsuominagi@gmail.com>
2021-05-18 12:51:26 -0500

    Added the package and the classes that I will need

---
+++ index.js
@@ -0,0 +1,13 @@
+// Included the packages that I will need on the system 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// Import the classes of Engineer,Manager and Intern into this file so I can use it
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+// Declare the variable that will save the information of the team
+
+const team = []
+
